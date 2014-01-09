2d1d717302a3586506b2ec2862525a59f8e24542
David Fisher <tibbon@gmail.com>
2014-01-09 11:59:53 -0500

    Create groupmaker.rb

---
+++ groupmaker.rb
@@ -0,0 +1,1 @@
+people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
