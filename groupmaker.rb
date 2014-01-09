b9eb839f2026c31c99bc8690e58e5365fb24f551
Prescott Murphy <prurph@gmail.com>
2014-01-09 14:30:11 -0500

    Made groups

--- groupmaker.rb
+++ groupmaker.rb
@@ -1,1 +1,15 @@
 people = ['Adi', 'Adrian', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Chloe', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Luke', 'Matt', 'Nick', 'Nicky', 'Ralph', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
+
+def mixer(array, groupsize)
+  shuffled_array = array.shuffle
+  num_groups = (array.length / groupsize).to_i
+  finished_groups = []
+
+  num_groups.times do
+    finished_groups << shuffled_array.shift(groupsize)
+  end
+
+  finished_groups << shuffled_array
+end
+
+# Returns 2D array of groups
